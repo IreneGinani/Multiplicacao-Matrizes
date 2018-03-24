c42981638e78d266d3021b5fccf730511333dfb3
ireneginani <ireneginani@gmail.com>
2018-03-24 15:43:38 -0300

    término da funcionalidade de leitura

--- leitura.py
+++ leitura.py
@@ -1,44 +1,54 @@
 import sys 
-param = sys.argv [1:]
-primeira_matriz = param[0]
-segunda_matriz = param[1]
 
-arquivo_matriz_1 = open(primeira_matriz, 'r')
-arquivo_matriz_2 = open(segunda_matriz, 'r')
-
-matriz_vetor = []
-matriz_1 = [[]]
-texto_matriz_1 = arquivo_matriz_1.readlines()
-contador = 0
-
-for linha in texto_matriz_1:
-	if (contador > 0):
-		contador_interno = 0 
-		
-		for elem in linha:
-
-			if ((contador_interno+1 < len(linha)) and (linha[contador_interno+1] != " ")):
-				if ((elem != " ") and (elem != '\n')):
-					matriz_vetor.append(int(elem+linha[contador_interno+1]))
-					elem = linha[contador+1]
-			elif ((elem != " ") and (elem != '\n')):
-				matriz_vetor.append(elem)
-				print ("fui")
-			
-			contador_interno += 1
-
-	matriz_1.append(matriz_vetor)
+def leitura(texto_matriz, matriz):
 	
-	contador += 1
+	matriz_vetor = []
+	contador = 0
 
-for linha in matriz_1:
-	for elem in linha:
-		print (elem)
+	for linha in texto_matriz:
+		if (contador > 0):
+			contador_interno = 0 
+			
+			for elem in linha:
+
+				if ((contador_interno+1 < len(linha)) and (linha[contador_interno+1] != " ")):
+					if ((elem != " ") and (elem != '\n')):
+						matriz_vetor.append(int(elem+linha[contador_interno+1]))
+				elif ((elem != " ") and (elem != '\n') and (linha[contador_interno+1] == " ") and (linha[contador_interno-1] == " ")):
+					matriz_vetor.append(int(elem))
+
+				contador_interno += 1
+
+		if (len(matriz_vetor) > 0):
+			matriz.append(matriz_vetor)
+			matriz_vetor = []
+		
+		contador += 1
+
+if __name__ == "__main__":
+
+	param = sys.argv [1:]
+	primeira_matriz = param[0]
+	segunda_matriz = param[1]
+
+	arquivo_matriz_1 = open(primeira_matriz, 'r')
+	arquivo_matriz_2 = open(segunda_matriz, 'r')
+
+	texto_matriz_1 = arquivo_matriz_1.readlines()
+	texto_matriz_2 = arquivo_matriz_2.readlines()
+
+	matriz_1 = []
+	matriz_2 = []
+
+	leitura(texto_matriz_1, matriz_1)
+	leitura(texto_matriz_2, matriz_2)
+
+	for linha in matriz_2:
+		print(linha)
+
+
+	
 
 
 
-
-
-
-texto_matriz_2 = arquivo_matriz_2.readlines()
 
